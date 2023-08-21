95771139f82460b71540a4ef0c2a504e2bb44a5f
MariLFO <mari.fer.onorio@gmail.com>
2023-08-21 12:30:02 -0700

    Session Beans for Entity Classes alteradas com Jakarta

---
+++ CadastroEE/CadastroEE-ejb/src/java/cadastroee/controller/PessoaFisicaFacade.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.controller;
+
+import cadastroee.model.PessoaFisica;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Mari
+ */
+@jakarta.ejb.Stateless
+public class PessoaFisicaFacade extends AbstractFacade<PessoaFisica> implements PessoaFisicaFacadeLocal {
+
+    @PersistenceContext(unitName = "CadastroEE-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PessoaFisicaFacade() {
+        super(PessoaFisica.class);
+    }
+    
+}
